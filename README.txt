93ad9d3d5a3ab62310ca5c825162a24d8aed9682
Allen Shaw <allen@JoineryHQ.com>
2021-12-14 13:00:15 -0600

    Version 8.x-1.0

--- README.txt
+++ README.txt
@@ -12,23 +12,45 @@ Licensed under [GNU GENERAL PUBLIC LICENSE, Version 2](LICENSE.txt)
 Into a given view (per configuration in Drupal's settings.php), which is expected
 to represent a single CiviCRM activity, inject functionality along these lines:
 
-* Assume the existence of, and identify, the html element 
-`<div id="cadetsviews-activity-status" data-cadetsviews-activity-status-id="N"/>`; 
-this element is expected to exist in the output of the given view.
-* Replace this element with 3 buttons, one for each of the possible statuses that 
+* Assume the existence of, and identify, the html element
+`<div id="cadetsviews-activity-status" data-cadetsviews-activity-status-id="N"/>`
+where N is the numeric ID of the activity status; this element is expected to
+exist in the output of the given view.
+* Replace this element with a collection of buttons, one for each of the possible statuses that
 may be held by the given activity.
-* Each button has 2 states: selected and unselected.  (These are indicated by 
-CSS class names, e.g. "status-selected" and "status-unselected".) 
+* Each button has 2 durable states: Selected and Unselected, as well as a temporary In-Progress state. (These are indicated by CSS class names; developers may wish to examine the DOM.)
 * Only one button will have the Selected state at any given time.
-* Upon loading the page, the button indicating the current status will have the 
-Selected state; the other 2 will be unselected.
-* The user may click (or tap) any unselected button to change the status; such action will 
-  * add a CSS class name to the button (e.g. "status-change-requested") indicating that it has been pressed and is awaiting a response; Justin may want to style such buttons in a way that provides feedback to the user regarding this "pending request" status.
-  * send a request via CiviCRM'S ajax api to change the activity status to that of the selected button, and wait for a response to this request.
-* Receipt of a response for this ajax-based status change request will trigger these changes:
-  * remove the above-mentioned CSS class name (e.g. "status-change-requested") from all buttons
-  * set the button states so that the currently saved status of the activity is reflected by assigning the selected state to only one button. 
+* Upon loading the page, the button indicating the current status will have the
+Selected state; the others will be unselected.
+* The user may click (or tap) any unselected button to change the status; such action will:
+  * Set all buttons to the In-Progress state, indicating that a button
+    has been pressed and we're awaiting a response.
+  * Send a request via CiviCRM'S ajax api to change the activity status to that
+    of the selected button, and wait for a response to this request.
+* Receipt of a response for this ajax-based status change request will trigger
+  these changes:
+  * Remove the In-Progress state from all buttons.
+  * Apply the Selected state to the button representing the currently saved status.
+  * Apply the Unselected state to other buttons.
 
+## Configuration:
+
+Configuration is defined through variables in Drupal's settings.php. Available
+configurations are these; all are required:
+
+```
+$config['cadetsviews'] = [
+  // The system ID of the view display on which we should inject the status buttons.
+  'medicalChartWithButtonsViewDisplayId' => '',           // For example, 'page_1'.
+
+  // The machine name of the view containing the above-named view display.
+  'medicalChartWithButtonsViewId' => '',                  // For example, 'civicrm_activity_test'
+
+  // CiviCRM activity status IDs for which buttons should be created. Buttons
+  // for each of these statuses will be displayed, in the order given here.
+  'medicalChartWithButtonsActivityStatusIds' => [],       // For example, [3,2,1]
+];
+```
 
 ## Support
 ![Joinery](/images/joinery-logo.png)
---
+++ js/medicalChartButtons.js
@@ -0,0 +1,109 @@
+CRM.$(function($) {
+  if (!drupalSettings.cadetsviews.activityId) {
+    // If activity id is not set, there's nothing we can do, so just exit.
+    return;
+  }
+
+  // Shorthand to drupal messaging api
+  var messages = new Drupal.Message();
+  // Global var for our own message Id. We only want to display one message at
+  // a time, so we'll remove this message before posting our next, if any.
+  var messageId;
+
+  /**
+   * Set all buttons to the correct state for a given status.
+   * @param Int statusId CiviCRM activity status id. If none given, all buttons are set to 'unselected' state.
+   * @returns void
+   */
+  var setButtonStatus = function setButtonStatus(statusId) {
+    // Mark all buttons as unselected.
+    $('div.cadetsviews-status-button').removeClass('cadetsviews-status-button-inprogress');
+    $('div.cadetsviews-status-button').removeClass('cadetsviews-status-button-selected');
+    $('div.cadetsviews-status-button').addClass('cadetsviews-status-button-unselected');
+    if (statusId) {
+      // Mark the given 'status' button as selected.
+      $('div.cadetsviews-status-button[data-cadetsviews-button-status-id="' + statusId +'"]').addClass('cadetsviews-status-button-selected');
+      $('div.cadetsviews-status-button[data-cadetsviews-button-status-id="' + statusId +'"]').removeClass('cadetsviews-status-button-unselected');
+    }
+  };
+
+  /**
+   * Click handler for all status buttons.
+   */
+  var statusButtonClick = function statusButtonClick(e) {
+    // if this button has in-progresss or selected classes, don't respond to clicks.
+    if (
+      $(this).hasClass('cadetsviews-status-button-inprogress') ||
+      $(this).hasClass('cadetsviews-status-button-selected')
+    ) {
+      return;
+    }
+
+    // Mark all buttons as in-progress
+    $('div.cadetsviews-status-button').addClass('cadetsviews-status-button-inprogress');
+
+    // update status with civicrm ajax api:
+    CRM.api4('Activity', 'update', {
+      where: [["id", "=", drupalSettings.cadetsviews.activityId]],
+      values: {"status_id":$(this).data('cadetsviews-button-status-id')},
+      reload: true,
+    }).then(function(result) {
+      handleStatusButtonAjaxResponse(result);
+    }, function(error) {
+      handleStatusButtonAjaxError(error);
+    });
+  };
+  
+  var setMessage = function setMessage(messageText, properties) {
+    if (typeof properties == 'undefined') {
+      properties = {};
+    }
+    // Remove any message we've posted.
+    if (messageId) {
+      messages.remove(messageId);
+    }
+    // Post this message with the given properties.
+    messageId = messages.add(messageText, properties);
+  };
+
+  /**
+   * Process ajax non-error response from civicrm ajax api.
+   */
+  var handleStatusButtonAjaxResponse = function handleStatusButtonAjaxResponse(result) {
+    // Get the newly defined status id; then 
+    // update button states accordingly
+    // and display a message that that the status was saved.
+    var updatedStatusId = result[0].status_id;
+    setButtonStatus(updatedStatusId);
+    setMessage('Status updated to: ' + drupalSettings.cadetsviews.statusLabels[updatedStatusId]);
+  };
+
+  /**
+   * Process ajax error response from civicrm ajax api.
+   */
+  var handleStatusButtonAjaxError = function handleStatusButtonAjaxError(error) {
+    // Display an error message.
+    setMessage(error.error_message, {type: 'error'});
+    // We don't know what to do now; to allow the user some control, set all buttons to "unselected".
+    setButtonStatus();
+  };
+
+  // Find the location where we should inject buttons.
+  var statusMarker = $('div#cadetsviews-activity-status');
+  // Note the current status Id.
+  var statusId = statusMarker.data('cadetsviews-activity-status-id');
+
+  // Insert all our buttons in order.
+  for (var i in drupalSettings.cadetsviews.statusIds) {
+    var buttonStatusId = drupalSettings.cadetsviews.statusIds[i];
+    var content = '<div class="cadetsviews-status-button cadetsviews-status-button-unselected" data-cadetsviews-button-status-id="' + buttonStatusId + '">' + drupalSettings.cadetsviews.statusLabels[buttonStatusId] + '</div>';
+    statusMarker.after(content);
+  }
+  // Define a click handler for all of our buttons.
+  $('div.cadetsviews-status-button').click(statusButtonClick);
+  // Remove the status marker as it's no longer needed.
+  statusMarker.remove();
+
+  // Set states for all buttons based on current status id.
+  setButtonStatus(statusId);
+});
