9c6d6d4c3ca1db6c8ff19db05e1a4fe93a86f072
cadets@aluminum <cadets@aluminum>
2023-09-22 16:29:36 +0000

    Fix asana 1205484629704855: download link placement, etc.

--- js/auditionhub.js
+++ js/auditionhub.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function ($) {
-  console.log('auditionhub.js');
+  // Move views result header below filter form.
+  jQuery('form.views-exposed-form').parent().find('header').insertAfter('form.views-exposed-form').show();
+  
+  // Determine a "csv download" url based on submitted filters (if any), and 
+  // insert a link to that url.
+  var insertCsvPath = '/audition.csv';
+  var url = new URL(window.location.href);
+  var csvTargetUrl = url.origin + url.pathname + insertCsvPath + url.search
+  jQuery('form.views-exposed-form').parent().find('header').prepend('<a id="auditionhub-csv-download-link" href="'+ csvTargetUrl +'" class="btn-Cadets-download"><i class="fa-regular fa-file-arrow-down"></i>&nbsp;Download</a>');
+
 });
